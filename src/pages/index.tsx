0b325fae806574a4aff40577b8c6c1b0d76c9029
Felipe <felipedemelopontes@gmail.com>
2021-03-23 18:14:42 +0100

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,27 @@
+import Head from 'next/Head'
+import { SubscribeButton } from '../components/SubscribeButton'
+import styles from './home.module.scss'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Home | ig.news</title>
+      </Head>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, Welcome!</span>
+          <h1>News about the <span>React</span> world</h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90</span>
+          </p>
+          <SubscribeButton />
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding"/>
+      </main>
+    </>
+  )
+}
