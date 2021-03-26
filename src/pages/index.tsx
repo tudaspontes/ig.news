eafd1888c3fc2c8909d0abfa1b476c8d2217b778
Felipe <felipedemelopontes@gmail.com>
2021-03-26 11:24:00 +0100

    feat: webhooks

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,19 @@
-import Head from 'next/Head'
+import { GetStaticProps } from 'next'
+import Head from 'next/head'
+
 import { SubscribeButton } from '../components/SubscribeButton'
+import { stripe } from '../services/stripe'
+
 import styles from './home.module.scss'
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  }
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -15,13 +26,31 @@ export default function Home() {
           <h1>News about the <span>React</span> world</h1>
           <p>
             Get access to all the publications <br />
-            <span>for $9.90</span>
+            <span>for {product.amount}</span>
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
 
         <img src="/images/avatar.svg" alt="Girl coding"/>
       </main>
     </>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve('price_1IYSToJ9Dn0Ptx9LbQNBhjbB')
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD'
+    }).format(price.unit_amount / 100,)
+  };
+
+  return {
+    props: {
+      product,
+    }
+  }
 }
