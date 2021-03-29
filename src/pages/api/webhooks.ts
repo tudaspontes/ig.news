e199a333597bd96c0ef7cfe7da1eb6d3efe5664d
Felipe <felipedemelopontes@gmail.com>
2021-03-29 18:05:30 +0200

    feat: manageSubs

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -1,15 +1,16 @@
 import { NextApiRequest, NextApiResponse } from "next"
-import { Readable } from "node:stream"
+import { Readable } from "stream"
 
 import Stripe from "stripe";
 import { stripe } from '../../services/stripe'
+import { saveSubscrition } from "./_lib/manageSubscription";
 
 async function buffer(readable: Readable) {
   const chunks = [];
 
   for await (const chunk of readable) {
     chunks.push(
-      typeof chunk === 'string' ? Buffer.from(chunk) : chunk
+      typeof chunk === "string" ? Buffer.from(chunk) : chunk
     );
   }
 
@@ -23,7 +24,9 @@ export const config = {
 }
 
 const relevantEvents = new Set([
-  'checkout.session.completed'
+  'checkout.session.completed',
+  'customer.subscription.updated',
+  'customer.subscription.deleted'  
 ])
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
@@ -41,13 +44,41 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 
     const { type } = event;
 
-    if(relevantEvents.has(type)) {
-      console.log('Evento recebido', event)
-    }
+    if (relevantEvents.has(type)) {
+      try {
+        switch (type) {
+          case 'customer.subscription.updated':
+          case 'customer.subscription.deleted':
+            const subscription = event.data.object as Stripe.Subscription;
 
-    res.json({ received:true })
+            await saveSubscrition(
+              subscription.id,
+              subscription.customer.toString(),
+              type === 'customer.subscription.created',
+            );
+
+            break;
+          case 'checkout.session.completed':
+            const checkoutSession = event.data.object as Stripe.Checkout.Session;
+
+            await saveSubscrition(
+              checkoutSession.subscription.toString(),
+              checkoutSession.customer.toString(),
+              true
+            );
+
+            break;
+          default:
+            throw new Error('Unhandled event.')
+        }
+      } catch (error) {
+        return res.json({ error: 'Webhook handler failed' })
+      }
+
+    res.json({ received: true })
   } else {
     res.setHeader('Allow', 'POST')
     res.status(405).end('Method not allowed')
+  }
   }
 }
